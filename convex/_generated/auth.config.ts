3726216fb6e74dca18f6f9c700afdcf4b3d11d9b
Chintan Udani <41039264+chintan-udani@users.noreply.github.com>
2024-01-10 11:24:37 +0530

    Create auth.config.ts

---
+++ convex/_generated/auth.config.ts
@@ -0,0 +1,10 @@
+/* trunk-ignore-all(prettier) */
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    providers: [
+      {
+        domain: "https://feasible-grouse-79.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
